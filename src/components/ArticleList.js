d5d55f7f38a05390373bd256fbe5e1c3eb5779a7
Justin Rodriguez <justin.r@me.com>
2023-08-03 15:45:39 -0400

    complete lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    const postsArray = posts.map(post => <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>)
+    return(
+        <main>
+            {postsArray}
+        </main>
+    )
+}
+
+export default ArticleList
